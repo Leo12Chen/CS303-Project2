f17727e4aa9778f211c52bc75006dc92a0d9a20c
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-11-23 11:52:56 -0600

    Update Folder.h

--- Folder.h
+++ Folder.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <string>
 #include <cmath>
+#include "File.h"
 using namespace std;
 
 class Folder {
 private:
 	string name;
 	double size;
+	File file;
 public:
 	Folder();
 	Folder(string n, double s) { name = n; size = s; };
+	void add_file(){}
 	void setname(string n) { name = n; };
 	string getname() { return name; };
 	void setsize(double s) { size = s; };
