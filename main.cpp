7b522e6eb9618d52200df8a2f4ede4d941e0e07d
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-11-23 11:10:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 
+
+#include "AVL_Tree.h"
+#include "Folder.h"
+#include "File.h"
+
+using namespace std;
+
+int main(){
+
+	AVL_Tree<Folder> AVL;
+
+	bool result = AVL.is_null();
+
+	Folder* fl = new Folder("X",2);
+  Folder* f2 = new Folder("y",3);
+  
+	AVL_Tree<Folder*> AVL_Folder;
+
+	AVL_Folder.insert(f1);
+
+  Folder* f = AVL_Folder.find(fl);
+
+	f->set_name("Z");
+
+
+	AVL_Folder.insert(f2);
+	AVL_Folder.erase("quick");
+
+	AVL_Tree<Folder*> right_subtree = AVL_Folder.get_right_subtree();
+
+	AVL_Tree<Folder*> left_subtree = AVL_Folder.get_left_subtree();
+
+
+	return 0;
+}
