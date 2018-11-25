01e7d03160e2af4afb45e401e5211f26a80ab5ca
armorsteven <43592967+armorsteven@users.noreply.github.com>
2018-11-25 11:51:00 -0600

    Update Folder.h

--- Folder.h
+++ Folder.h
@@ -14,9 +14,37 @@ private:
 public:
 	Folder();
 	Folder(string n, double s) { name = n; size = s; };
+	
 	void add_file(File file) {
 		ListOfFile.push_back(file);
 		size += file.getsize();
+	}
+	
+	void add_folder(string path, string folder_name)
+	{
+		int folder_size = ListOfFolder.size();
+		Folder new_folder(folder_name, 0);
+		string s = path + "/";
+		string delimiter = "/";
+		size_t pos = 0;
+		string token;
+
+		while ((pos = s.find(delimiter)) != string::npos) {
+			token = s.substr(0, pos);
+			s.erase(0, pos + delimiter.length());
+
+			for (int i = 0; i != folder_size; i++)
+			{
+				if (token == ListOfFolder[i].getname() && s == "")
+					ListOfFolder[i].ListOfFolder.push_back(new_folder);
+
+				else if (token == ListOfFolder[i].getname() && s != "")
+					folder_size = ListOfFolder[i].ListOfFolder.size();
+
+				else
+					cout << "Path Does Not Exist!" << endl;
+			}
+		}
 	}
 	
 	void setname(string n) { name = n; };
