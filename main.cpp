62f18289a6c15f450a95cb4b0ca373d7dfd4b80d
armorsteven <43592967+armorsteven@users.noreply.github.com>
2018-11-26 12:34:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ By Leo Chen and Steven Hoang
 Project 2A
 ############################################################################################################
 Basic Algorithm for the Project:
-For the whole project we have the Folder class and File class, 
+For the whole project we have the Folder class and File class,
 Folder is able to contain folders and files, they are stored in
 two vectors. And we will do the search by the avl tree.
 First we have the function to decode the path, which we split the
-path by '/'.It is able to add folders and files, delete folders 
+path by '/'.It is able to add folders and files, delete folders
 and files and get the list of files. This main.cpp baiscly is the
 simulator for the project. Have to test it manually.
 ############################################################################################################
@@ -40,7 +40,7 @@ vector<string> split(string path) {//here is the function to decode the path
 }
 void add_folders(AVL_Tree<Folder*>& avl, string path, string folder_name) //function for add folder
 {
-	Folder new_folder(folder_name,0);
+	Folder new_folder(folder_name, 0);
 	string s = path + "/";
 	string delimiter = "/";
 	size_t pos = 0;
@@ -50,14 +50,14 @@ void add_folders(AVL_Tree<Folder*>& avl, string path, string folder_name) //func
 		token = s.substr(0, pos);
 		s.erase(0, pos + delimiter.length());
 	}
-	Folder* find_folder = new Folder(token,0);
+	Folder* find_folder = new Folder(token, 0);
 	Folder* temp = avl.find(find_folder);
 	temp->ListOfFolder.push_back(new_folder);
 }
 
 void delete_folders(AVL_Tree<Folder*>& avl, string path, string folder_name) //function for delete folder
 {
-	Folder new_folder(folder_name,0);
+	Folder new_folder(folder_name, 0);
 	string s = path + "/";
 	string delimiter = "/";
 	size_t pos = 0;
@@ -67,7 +67,7 @@ void delete_folders(AVL_Tree<Folder*>& avl, string path, string folder_name) //f
 		token = s.substr(0, pos);
 		s.erase(0, pos + delimiter.length());
 	}
-	Folder* find_folder = new Folder(token,0);
+	Folder* find_folder = new Folder(token, 0);
 	Folder* temp = avl.find(find_folder);
 	temp->ListOfFolder.pop_back();
 }
@@ -89,7 +89,7 @@ void add_file(string path, string name, double size, vector<Folder> &f) {//funct
 
 File get_files(AVL_Tree<File*>& avl, string path, string file_name)  //function for get files
 {
-	File new_file(file_name,0);
+	File new_file(file_name, 0);
 	string s = path + "/";
 	string delimiter = "/";
 	size_t pos = 0;
@@ -99,7 +99,7 @@ File get_files(AVL_Tree<File*>& avl, string path, string file_name)  //function 
 		token = s.substr(0, pos);
 		s.erase(0, pos + delimiter.length());
 	}
-	File* find_file = new File(token,0);
+	File* find_file = new File(token, 0);
 	File* re_file = avl.find(find_file);
 	return *re_file;
 
@@ -110,7 +110,7 @@ File get_files(AVL_Tree<File*>& avl, string path, string file_name)  //function 
 void delete_files(AVL_Tree<File*>& avl, string path, string file_name) //function for delete files
 {
 
-	File new_file(file_name,0);
+	File new_file(file_name, 0);
 	string s = path + "/";
 	string delimiter = "/";
 	size_t pos = 0;
@@ -120,7 +120,7 @@ void delete_files(AVL_Tree<File*>& avl, string path, string file_name) //functio
 		token = s.substr(0, pos);
 		s.erase(0, pos + delimiter.length());
 	}
-	File* find_file = new File(token,0);
+	File* find_file = new File(token, 0);
 	File* temp = avl.find(find_file);
 	temp = NULL;
 
@@ -182,7 +182,7 @@ void delete_file(string path, string name, vector<Folder> &f) {// another delete
 }
 
 
-void get_file(string path, string file_name, vector<Folder>&f){
+void get_file(string path, string file_name, vector<Folder>&f) {
 	vector<string> foldersname = split(path);
 	for (int i = 0; i < f.size(); i++) {
 		if (f[i].getname() == foldersname[foldersname.size() - 1]) {
@@ -200,16 +200,20 @@ int main() {                          //the simulator
 	folders.push_back(root);
 	int choice;
 	int counter = 0;
-	cout << "Welcome to our awsome file system" << endl;
+	cout << "Welcome to our awesome file system!" << endl;
+	cout << "We have already created a root folder for you." << endl << endl;
+	cout << "Hit the 'Enter' key to continue..." << endl;
+	cin.ignore();
+	system("CLS");
 
 	while (counter == 0) {
-		cout << "We have already created a root folder for you, please start your path with root/.../..." << endl;
-		cout << "Now tell me what you want to do" << endl;
-		cout << "1. add new folder" << endl;
-		cout << "2. add new file" << endl;
-		cout << "3. delete folder" << endl;
-		cout << "4. delete file" << endl;
-		cout << "5. show all files under a specific folder by enter path" << endl;
+		cout << endl << "please start your path with root/ ... / ..." << endl << endl;
+		cout << "Now tell me what you want to do:" << endl;
+		cout << "1. Add new folder" << endl;
+		cout << "2. Add new file" << endl;
+		cout << "3. Delete folder" << endl;
+		cout << "4. Delete file" << endl;
+		cout << "5. Show all files under a specific folder by enter path" << endl;
 		cout << "6. Show information of all folders!" << endl;
 		cout << "7. Quit" << endl;
 		cin >> choice;
@@ -217,12 +221,13 @@ int main() {                          //the simulator
 		case 1: {
 			string path;
 			string name;
-			cout << "What is path? Please becareful and follow the format" << endl;
+			system("CLS");
+			cout << "What is path? Please be careful and follow the format" << endl;
 			cin >> path;
-			cout << "What is the name of the folder?" << endl;
+			cout << endl << "What is the name of the folder?" << endl;
 			cin >> name;
 			add_folder(path, name, folders);
-			cout << "The folder " << name << " under path " << path << " has been added" << endl;
+			cout << endl << "The folder " << name << " under path " << path << " has been added" << endl << endl;
 
 			break;
 		}
@@ -230,42 +235,47 @@ int main() {                          //the simulator
 			string path;
 			string name;
 			double size;
-			cout << "What is path? Please becareful and follow the format" << endl;
+			system("CLS");
+			cout << "What is path? Please be careful and follow the format" << endl;
 			cin >> path;
-			cout << "What is the name of the file?" << endl;
+			cout << endl << "What is the name of the file?" << endl;
 			cin >> name;
-			cout << "What is the size of the file?" << endl;
+			cout << endl << "What is the size of the file?" << endl;
 			cin >> size;
 			add_file(path, name, size, folders);
-			cout << "The file " << name << " with size of " << size << " under path " << path << " has been added." << endl;
+			cout << endl << "The file " << name << " with size of " << size << " under path " << path << " has been added." << endl;
 			break;
 		}
 		case 3: {
 			string path;
 			string name;
-			cout << "What is path? Please becareful and follow the format" << endl;
+			system("CLS");
+			cout << "Make sure it is the full path! if you want to remove door in the path 'root/door', the path must be 'root/door'!" << endl << endl;
+			cout << "What is path? Please be careful and follow the format" << endl;
 			cin >> path;
-			cout << "What is the name of the folder?" << endl;
+			cout << endl << "What is the name of the folder?" << endl;
 			cin >> name;
 			delete_folder(path, name, folders);
-			cout << "The folder " << name << " under path " << path << " has been removed" << endl;
+			cout << endl << "The folder " << name << " under path " << path << " has been removed" << endl;
 			break;
 		}
 		case 4: {
 			string path;
 			string name;
-			cout << "What is path? Please becareful and follow the format" << endl;
+			system("CLS");
+			cout << "What is path? Please be careful and follow the format" << endl;
 			cin >> path;
-			cout << "What is the name of the file?" << endl;
+			cout << endl << "What is the name of the file?" << endl;
 			cin >> name;
 			delete_file(path, name, folders);
-			cout << "The file " << name << " under path " << path << " has been removed" << endl;
+			cout << endl << "The file " << name << " under path " << path << " has been removed" << endl;
 			break;
 		}
 		case 5: {
 			string path;
 			vector<File> tf;
-			cout << "What is path? Please becareful and follow the format" << endl;
+			system("CLS");
+			cout << "What is path? Please be careful and follow the format" << endl;
 			cin >> path;
 			tf = get_files(path, folders);
 			break;
